056fb461d54b3765f7a81d2af46673b173d8c8a4
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-08-13 16:23:28 +0000

    first commit

---
+++ app/donate/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Donate</div>
+  )
+}
+
+export default page
---
+++ app/ourstore/educators/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Educators
+    </div>
+  )
+}
+
+export default page
